3a9ff3a40ba74e45171ac5a1e47c5bd248eabc42
Cyber <26813031+Cyber-EX@users.noreply.github.com>
2024-10-09 15:23:54 +0300

    Update script.js

--- qr/script.js
+++ qr/script.js
@@ -15,7 +15,14 @@ domReady(function () {
 
     // If found you qr code
     function onScanSuccess(decodeText, decodeResult) {
-        alert("You Qr is : " + decodeText, decodeResult);
+        alert("Your Qr is : " + decodeText, decodeResult);
+
+        var fileContent = decodeText;
+        var bb = new Blob([fileContent ], { type: 'text/plain' });
+        var a = document.createElement('a');
+        a.download = 'download.txt';
+        a.href = window.URL.createObjectURL(bb);
+        a.click();
     }
 
     let htmlscanner = new Html5QrcodeScanner(
